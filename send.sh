f92171bb4e91328b7354578a65ded889a40db7c7
Fusl <fusl@meo.ws>
2015-09-04 14:44:48 +0200

    Fix parameter shift and rewrite a large part of the script

--- send.sh
+++ send.sh
@@ -9,52 +9,44 @@ PASSWD=XXXXX
 # ~~~~~  CONFIG END  ~~~~~ #
 # Do not edit anything below this line! #
 
-# grab text, dest number and sending application
+urlencode() {
+	# Usage: urlencode "<string>"
+	local length="${#1}"
+	for (( i = 0; i < length; i++ )); do
+		local c="${1:i:1}"
+		case $c in
+			[a-zA-Z0-9.~_-]) printf "$c" ;;
+			*) printf '%%%02X' "'$c"
+		esac
+	done
+}
+
 NUMBER="$1"; shift
-SENDER="$2"; shift
+SENDER="$1"; shift
 TEXT="$*"
 
-#### dest number input validation and fixes ####
-# various number fixes
-# change + in number to 00 if it exists
-NUMBER=$(echo $NUMBER | sed -e "s/+/00/")
+NUMBER=$(echo "$NUMBER" | sed -r 's/^00/+/;s/[^0-9]//g');
 
-# number is fully numeric now, check this
-if echo $NUMBER | grep "^[0-9]*$" >/dev/null
-then
-	echo "number numeric - ok" >> /dev/null
-else
-	echo "number not numeric - please fix"
-	exit 1
+if ! echo "$SENDER" | egrep -q '^[0-9]{1,11}$'; then
+	if ! echo "$SENDER" | egrep -q '^.{1,16}$'; then
+		echo "Invalid sender number or name"
+		exit 1
+	fi
 fi
 
-# change 00 in number to needed format for GET request (replace 00 with %2B (+))
-NUMBER=$(echo $NUMBER | sed -e "s/00/%2B/")
+#######################################################################
+# The maximum length of text message that you can send is 918         #
+# characters. However, if you send more than 160 characters then your #
+# message will be broken down in to chunks of 153 characters before   #
+# being sent to the recipient's handset.                              #
+#######################################################################
 
-#### /dest number input validation ####
+LOGIN=$(urlencode "$LOGIN")
+PASSWD=$(urlencode "$PASSWD")
+NUMBER=$(urlencode "$NUMBER")
+SENDER=$(urlencode "$SENDER")
 
-#### sendername validation and fixes ####
-# check if string is number or text
-if echo $SENDER | grep "^[0-9]*$" >/dev/null
-then
-	#string is text, allow 11 characters
-	SENDER=$(echo $SENDER | cut -c1-11)
-else
-	#string is number, allow 16 numbers
-	SENDER=$(echo $SENDER | cut -c1-16)
-fi
-
-# replace empty strings with 0s
-SENDER=$(echo $SENDER | sed -e "s/ /0/")
-
-#### /sendername validation and fixes ####
-
-#### text validation and fixes ####
-# cut the text down to 150 chars max
-TEXT=$(echo $TEXT | cut -c1-150)
-# replace space with +
-TEXT=$(echo $TEXT | sed -e "s/ /+/g")
-#### /text validation and fixes ####
-# checks completed, curl the gateway and send the SMS
-
-curl -s "http://www.http2sms.com/sms/?id=$LOGIN&pw=$PASSWD&dnr=$NUMBER&snr=$SENDER&msg=$TEXT"
+for seek in $(seq 0 918 "${#TEXT}"); do
+	TEXTPART=$(urlencode "${TEXT:seek:918}")
+	echo curl -s "http://www.http2sms.com/sms/?id=$LOGIN&pw=$PASSWD&dnr=$NUMBER&snr=$SENDER&msg=$TEXT"
+done
